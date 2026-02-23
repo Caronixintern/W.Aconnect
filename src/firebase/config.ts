548c454f7b50634490ea735b5933ef82a4c0261f
Careonix <careonixintern@gmail.com>
2026-02-23 11:08:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-149235123-ff1e0",
+  "appId": "1:103767282395:web:f4d361757d470908f732fe",
+  "apiKey": "AIzaSyC1Ftyq5EEt9heBn-5HKQlZPENtX2aUAVY",
+  "authDomain": "studio-149235123-ff1e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "103767282395"
+};
